6e27722777119b78ae999cfd02ff2d9b8227697a
natael <nataelmf@gmail.com>
2022-11-05 11:20:45 +0100

    ft(timers): Upload timers.c to change O.S

--- timer.c
+++ timer.c
@@ -1,1 +1,27 @@
+/**
+ * @file timer.c
+ * @author Alejandro
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-05
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
 
+volatile int mark = 0;
+
+void trigger(int signal)
+{
+    mark = 1;
+}
+
+int main()
+{
+    signal(SIGALRM, trigger);
+    alarm(3);
+
+}
