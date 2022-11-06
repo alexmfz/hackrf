d048c4d9423ff7e8e0ffd3135b8f05dfd520c90a
AMFZ <nataelmf@gmail.com>
2022-11-06 14:35:46 +0100

    ft(timer.c): Rename functions and logs errors

--- timer.c
+++ timer.c
@@ -15,55 +15,71 @@
 #include <sys/time.h>
 
 #define ITERATIONS  10
-#define INTERVAL 250000 //us
+#define INTERVAL 25e4 //us => 25e4 us = 250 ms
 #define MICRO   1e6
+
 volatile int mark = 0;
+
+struct itimerval timer;    
+struct timeval timeval;
+float durationIteration = 0;
 
 void timerHandler(int sig)
 {
     mark = 1;
 } 
 
-void iteration(long previousTime, long postTime)
+void setTimerParams()
 {
-    struct itimerval timer;    
-    struct timeval timeval;
-
-    gettimeofday(&timeval,NULL);
-    previousTime = timeval.tv_sec*1e6 + timeval.tv_usec;
-    printf("Time:%ld\n", previousTime);
-
-    if(signal(SIGALRM, timerHandler) == SIG_ERR)
-    {
-        perror("Unable to catch alarm signal");
-        return ;
-    }
     timer.it_value.tv_sec = 0;
     timer.it_value.tv_usec = INTERVAL;
     timer.it_interval = timer.it_value;
+}
+
+float hackRFTrigger(long previousTime, long postTime)
+{
+    gettimeofday(&timeval,NULL);
+    previousTime = timeval.tv_sec*1e6 + timeval.tv_usec;
+    printf("hackRFTrigger | Initial time:%ld\n", previousTime);
+
+    if(signal(SIGALRM, timerHandler) == SIG_ERR)
+    {
+        perror("hackRFTrigger | Unable to catch alarm signal");
+        return 0;
+    }
+   
     if(setitimer(ITIMER_REAL, &timer, 0) == -1)
     {
-        perror("Error calling timer");
-        return ;
+        perror("hackRFTrigger | Error calling timer");
+        return 0;
     }
     
     while(!mark){ }
     gettimeofday(&timeval,NULL);
     postTime = timeval.tv_sec*1e6 + timeval.tv_usec;
-    printf("Time:%ld\n", postTime);
-    printf("Duration: %.2f s\n", (float)(postTime-previousTime)/1e6);
-    
+
+    durationIteration = (float)(postTime-previousTime)/MICRO;
     mark = 0;  
+
+    printf("hackRFTrigger | Final time:%ld\n", postTime);
+    printf("hackRFTrigger | Duration: %.2f s\n", (float)(postTime-previousTime)/MICRO);
+    
+    return durationIteration;
 }
 
 int main()
 {
     long previousTime = 0 , postTime = 0;
- 
+    float totalDuration = 0;
+    
+    setTimerParams();
+
     for(int i = 0; i < ITERATIONS; i++)
     {
         printf("Iteration %d:\n", i+1);
-        iteration(previousTime, postTime);
+        totalDuration += hackRFTrigger(previousTime, postTime);
     }
+
+    printf("\n\nIterations: %d\nTotal duration: %f s\n", ITERATIONS, totalDuration);
     return 0;    
 }
