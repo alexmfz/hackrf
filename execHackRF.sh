009a8621c32fcf8f46fcc876fb52037b2b2246f6
natael <nataelmf@gmail.com>
2022-11-16 18:47:33 +0100

    Updated exec bash file

--- execHackRF.sh
+++ execHackRF.sh
@@ -1,8 +1,7 @@
-for i in {1. .3600}
-do
-echo "Iteration $i started"
-
-./hackrf_sweep -f45:245 -w1000000 -1
-
-echo "Iteration $i success"
-done
+for i in {1..100}
+    do
+        echo "Iteration $i. started"
+        ./hackrf_sweep-original -f45:245 -w1000000 -1
+        echo "Iteration $i. success"
+        sleep 1
+    done
