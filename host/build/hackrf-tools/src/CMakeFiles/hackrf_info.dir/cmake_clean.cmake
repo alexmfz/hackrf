6069d4a1c6c7c52a65fb91957f0bd0c7d8a78971
alexmfz <47566168+alexmfz@users.noreply.github.com>
2022-11-16 20:01:57 +0100

    Add hackrf_info

---
+++ host/build/hackrf-tools/src/CMakeFiles/hackrf_info.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hackrf_info.dir/hackrf_info.c.o"
+  "hackrf_info"
+  "hackrf_info.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hackrf_info.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
