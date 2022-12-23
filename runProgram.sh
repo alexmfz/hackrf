422e055643320120c46a20ea0b714810b0b5659b
natael <nataelmf@gmail.com>
2022-12-23 12:31:15 +0100

    Updated version of C & shell scripts to v3.5

--- runProgram.sh
+++ runProgram.sh
@@ -1,10 +1,10 @@
 
 #!/bin/bash
-if  [[ -z "$1" || -z "$2" ]]
+if  [[ -z "$1" || -z "$2"]]
 then
     echo "Was not possible to execute."
     echo "Frequency range values not selected"
-    echo "Example: './runProgram.sh 45 245'"
+    echo "Example: './runProgram.sh 45 245' [0 (C generation) or 1 (python generation)] "
 else
     echo "...Running Compiler..."
     
@@ -14,16 +14,29 @@ else
     echo "...Compiler Finished..."
     
     echo "...Running Program..."
-    
+
     cd /home/manolo/hackrf/host/hackrf-tools/src/FitsFolder
-    ./hackrf_sweep -f$1:$2
-    
+
+    if [ "$3" -eq 0 ]
+    then
+      echo "...Fits file will be generate with C script..."
+      ./hackrf_sweep -f$1:$2 -p$3
+      echo "...Moving fits into Result folder"
+      mv *.fit Result/
+    else
+      echo "...Fits file will be generate with Python script..."
+      ./hackrf_sweep -f$1:$2 -p$3
+      mv samples.txt /pythonScripts
+      mv times.txt /pythonScripts
+      mv frequencies.txt /pythonScripts
+      cd pythonScripts
+      python3 generationFits.py
+      echo "...Moving fits into Result folder"
+      mv *.fit /home/manolo/hackrf/host/hackrf-tools/src/FitsFolder/Result
+    fi
+
     echo "...Program Finished..."
-    echo "...Moving fits file into Result Folder"
-    
-    mv *.fit Result/
     echo "...Opening JavaViewer..."
-    
     cd Result
     java -jar RAPPViewer.jar
 fi
