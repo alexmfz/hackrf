959f9abd8731d196f183a8ffb9ce92099a72bc99
natael <nataelmf@gmail.com>
2022-12-29 20:43:57 +0100

    Updated to v3.8.1 (runCompiler.sh can run everywhere)

--- runCompiler.sh
+++ runCompiler.sh
@@ -1,51 +1,53 @@
 
+originalPath=$(pwd)
+echo $originalPath
 #Remove previous files .c.o, .o
-cd /home/manolo/hackrf/host/build/hackrf-tools/src
+cd $originalPath/host/build/hackrf-tools/src/
+echo $(pwd)
 rm hackrf_sweep
-rm hackrf_info
 
-cd /home/manolo/hackrf/host/hackrf-tools/src/FitsFolder
-rm samples.txt
-rm header_times.txt
-rm times.txt
-rm frequencies.txt
-rm hackrf_sweep
-rm hackrf_info
-echo "Executable hackrf_sweep successfully removed"
-echo "Executable hackrf_info successfully removed"
-
-cd /home/manolo/hackrf/host/build/hackrf-tools/src/CMakeFiles/hackrf_sweep.dir
+cd CMakeFiles/hackrf_sweep.dir
 rm generationFits.c.o 
 rm functions.c.o
 rm timer.c.o
 echo "Files object successfully removed"
 
+
+cd $originalPath/host/hackrf-tools/src/FitsFolder/
+rm samples.txt
+rm header_times.txt
+rm times.txt
+rm frequencies.txt
+rm hackrf_sweep
+
+echo "Executable hackrf_sweep successfully removed"
+
 ###Compile###
 #Fits functions
-cd /home/manolo/hackrf/host/hackrf-tools/src/FitsFolder/
 gcc -c generationFits.c -o generationFits.c.o -I usr/local/cfitsio-4.1.0 -lcfitsio
-cp -R generationFits.c.o /home/manolo/hackrf/host/build/hackrf-tools/src/CMakeFiles/hackrf_sweep.dir
+cp -R generationFits.c.o $originalPath/host/build/hackrf-tools/src/CMakeFiles/hackrf_sweep.dir
 echo "Fits functions successfully compiled"
 
 #External API functions
-cd /home/manolo/hackrf/host/hackrf-tools/src/Functions/
+cd $originalPath/host/hackrf-tools/src/Functions/
 gcc -c functions.c -o functions.c.o
-cp -R functions.c.o /home/manolo/hackrf/host/build/hackrf-tools/src/CMakeFiles/hackrf_sweep.dir
+cp -R functions.c.o $originalPath/host/build/hackrf-tools/src/CMakeFiles/hackrf_sweep.dir
 echo "Extern API functions successfully compiled"
 
 #Timer functions
-cd /home/manolo/hackrf/host/hackrf-tools/src/Timer/
+cd $originalPath/host/hackrf-tools/src/Timer/
 gcc -c timer.c -o timer.c.o
-cp -R timer.c.o /home/manolo/hackrf/host/build/hackrf-tools/src/CMakeFiles/hackrf_sweep.dir
+cp -R timer.c.o $originalPath/host/build/hackrf-tools/src/CMakeFiles/hackrf_sweep.dir
 echo "Extern timers functions successfully compiled"
+
 #Main folder
-cd /home/manolo/hackrf/host/build/hackrf-tools/src/
+cd $originalPath/host/build/hackrf-tools/src/
 
 make
 echo "All files were successfully compiled"
 
-cp -R /home/manolo/hackrf/host/build/hackrf-tools/src/hackrf_sweep /home/manolo/hackrf/host/hackrf-tools/src/FitsFolder/
-cp -R /home/manolo/hackrf/host/build/hackrf-tools/src/hackrf_info /home/manolo/hackrf/host/hackrf-tools/src/FitsFolder/
+cp -R $originalPath/host/build/hackrf-tools/src/hackrf_sweep $originalPath/host/hackrf-tools/src/FitsFolder/
+cp -R $originalPath/host/build/hackrf-tools/src/hackrf_info $originalPath/host/hackrf-tools/src/FitsFolder/
 
 echo "Ready to exec"
 
