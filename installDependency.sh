fb47ba8c3c9a1fd3b8a869ecfb3b556e6ddf2eb4
natael <nataelmf@gmail.com>
2023-01-05 09:32:58 +0100

    Updated installDependendy.sh. Version 4.1

--- installDependency.sh
+++ installDependency.sh
@@ -1,13 +1,16 @@
 echo "...Downloading dependencies..."
-
-sudo apt-get install libfftw3-dev
-sudo apt-get install libusb-1.0
-sudo apt-get install cmake
-sudo apt-get install make
-sudo apt-get install gcc
-sudo apt-get install hackrf
-sudo apt install zlib1g
-sudo apt install zlib1g-dev
+sudo apt-get --yes update
+sudo apt-get --yes install libfftw3-dev
+sudo apt-get --yes install libusb-1.0
+sudo apt-get --yes install cmake
+sudo apt-get --yes install make
+sudo apt-get --yes install gcc
+sudo apt-get --yes install hackrf
+sudo apt --yes install zlib1g
+sudo apt --yes install zlib1g-dev
+sudo apt-get --yes install default-jdk
+sudo apt-get --yes install default-jre
+sudo apt --yes autoremove
 
 wget -O cfitsio-3.47.tar.gz https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-3.47.tar.gz
 
