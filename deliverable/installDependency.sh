ab5d022d43c37e19b37ec1635322c652a8056143
natael <nataelmf@gmail.com>
2023-01-08 23:15:14 +0100

    Upload deliverable folder

---
+++ deliverable/installDependency.sh
@@ -0,0 +1,29 @@
+echo "...Downloading dependencies..."
+sudo apt-get --yes update
+sudo apt-get --yes install libfftw3-dev
+sudo apt-get --yes install libusb-1.0
+sudo apt-get --yes install cmake
+sudo apt-get --yes install make
+sudo apt-get --yes install gcc
+sudo apt-get --yes install hackrf
+sudo apt --yes install zlib1g
+sudo apt --yes install zlib1g-dev
+sudo apt-get --yes install default-jdk
+sudo apt-get --yes install default-jre
+sudo apt --yes autoremove
+
+wget -O cfitsio-3.47.tar.gz https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-3.47.tar.gz
+
+tar xvf cfitsio-3.47.tar.gz
+rm cfitsio-3.47.tar.gz
+cd cfitsio-3.47
+
+echo "...Installing dependencies..."
+sudo ./configure --prefix=/usr/local
+sudo make
+sudo make install
+sudo make clean
+sudo ldconfig
+
+
+echo "...Dependencies successfully installed..."
