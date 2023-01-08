dddea9489d5384365ce866947e9b058583f55ea2
alexmfz <nataelmf@gmail.com>
2023-01-08 16:53:56 +0100

    Comparison dates with bash

--- reader.sh
+++ reader.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 filename='scheduler.cfg'
 
-n=1
-while read line;
+time_now=$(date +%H%M%S)
+
+while read schedule_time;
 do
 	# read each line
-	echo "Line No. $n: $line"
-	n=$((n+1))
+	#echo "time scheduled "$line
+	#echo "time now:" $time_now
+	#echo "time scheduled:" $schedule_time
+			
+	if [ $time_now -gt $schedule_time ]
+	then
+		echo "Cannot be scheduled on the pass"
+	else
+		echo "Executing script"
+	fi
+	
 done < $filename
